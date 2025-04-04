9a77d3b8c8011df2c2995bf72ba3e3dc55de0270
Chetan (TCR) Sapkal <68210482+ceotcr@users.noreply.github.com>
2025-04-04 21:43:24 +0530

    Add error handling to server routes and return appropriate status codes

--- node-basic/DAY05/server.js
+++ node-basic/DAY05/server.js
@@ -7,14 +7,23 @@ import { genresRouter } from './routers/genresRouter.js';
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-    if (req.url.startsWith('/api/users')) {
-        usersRouter(req, res);
-    } else if (req.url.startsWith('/api/books')) {
-        booksRouter(req, res);
-    } else if (req.url.startsWith('/api/authors')) {
-        authorsRouter(req, res);
-    } else if (req.url.startsWith('/api/genres')) {
-        genresRouter(req, res);
+    try {
+        if (req.url.startsWith('/api/users')) {
+            usersRouter(req, res);
+        } else if (req.url.startsWith('/api/books')) {
+            booksRouter(req, res);
+        } else if (req.url.startsWith('/api/authors')) {
+            authorsRouter(req, res);
+        } else if (req.url.startsWith('/api/genres')) {
+            genresRouter(req, res);
+        }
+        else {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ error: 'Not Found' }));
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Internal Server Error' }));
     }
 });
 
